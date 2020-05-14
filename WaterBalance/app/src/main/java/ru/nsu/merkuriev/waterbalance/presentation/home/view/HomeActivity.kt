b23b67a34c91e4793d8f9a469aef6dae52274da0
zahar_merkuriev <zahar.merkuriev@gmail.com>
2020-05-14 19:11:54 +0700

    add start screen

---
+++ WaterBalance/app/src/main/java/ru/nsu/merkuriev/waterbalance/presentation/home/view/HomeActivity.kt
@@ -0,0 +1,23 @@
+package ru.nsu.merkuriev.waterbalance.presentation.home.view
+
+import androidx.activity.viewModels
+import ru.nsu.merkuriev.waterbalance.R
+import ru.nsu.merkuriev.waterbalance.databinding.ActivityHomeBinding
+import ru.nsu.merkuriev.waterbalance.presentation.common.view.BaseToolbarActivity
+import ru.nsu.merkuriev.waterbalance.presentation.home.viewmodel.HomeViewModel
+import ru.terrakok.cicerone.Navigator
+import ru.terrakok.cicerone.android.pure.AppNavigator
+
+class HomeActivity : BaseToolbarActivity<ActivityHomeBinding>() {
+
+    override val navigator: Navigator = AppNavigator(this, 0)
+
+    override fun getLayoutId(): Int = R.layout.activity_home
+
+    override val viewModel: HomeViewModel by viewModels { viewModelFactory }
+
+    override fun initToolbar() {
+        super.initToolbar()
+        setToolbarTitle(R.string.home_screen_title)
+    }
+}
